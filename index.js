80ac5d4a8622d0b0c5846152b631450953a19da8
Victor Alves <victoralvescabral7@gmail.com>
2024-07-03 08:52:30 -0300

    finish the TS basics

--- index.js
+++ index.js
@@ -40,4 +40,60 @@ let jackOccupation = "employed";
 "employed" or the string "student", if the result of a variable that uses the occupation type is different than those two, we have an error!
 */
 // -------------------------------------------------------------------------------------------------------------------------------------------------------------------------------//
-// TYPE ASSERTION ->
+// TYPE ASSERTION -> Let's say we create a variable called y with the type of any, that initially is a string. With Type Assertion we are able to specify the type of the change!
+let y = "a";
+y = 22;
+// -------------------------------------------------------------------------------------------------------------------------------------------------------------------------------//
+// FUNCTIONS ->
+// How to write function that returns something -> the parameters should have their type specified, and after that we need to write what type will be the result of the function;
+function add(x, y) {
+    return x + y;
+}
+add(2, 5);
+// How to write function that don't return anything ->
+function sayHi(msg) {
+    console.log(msg);
+}
+sayHi("hey");
+// -------------------------------------------------------------------------------------------------------------------------------------------------------------------------------//
+// GENERICS ->
+/* In this case we are creating a function that takes an array and returns a copy of that array. We added three T's, which are things called "generics", creating a...
+placeholder to the function called T(it can be anything). So when we create the numCopy and strCopy variables, inside it we call the function, and in the place of the T, we set the...
+type(T) to the type in question, followed by the parameter(array with T's, where in numCopy is a number, and in strCopy is a string!);
+
+ps -> the first T is to create the placeholder, the second one is to specify the type of the array which is an array filled with T, and of course the third T is the result, an array...
+filled with T;
+
+*/
+function copyArr(arr) {
+    return [...arr];
+}
+let numCopy = copyArr([1, 2, 3]);
+let strCopy = copyArr(["1", "2", "3"]);
+// -------------------------------------------------------------------------------------------------------------------------------------------------------------------------------//
+/* ENUMS(enumerations) -> A feature in TypeScript that allows you to define a set of named constants in a object. So we start creatin an enum object called EyeColor with colors as...
+keys inside the object. We are going to create a function that displays in the console a student given information(including the eye color). So we create the function displayStudent...
+getting as a parameter, a "student" with the type of Student(interface), that is an object filled with three different properties, name as a string, age as a number and EyeColor as...
+the EyeColor enum object. So back to the displayStudent function we console.log the three expected properties, we call the function passing an object with the three expected keys, the...
+eyeColor specified as the brown key inside the EyeColor enum object! And the result at the console log is:
+
+Jack
+20
+Common eye color
+
+*/
+var EyeColor;
+(function (EyeColor) {
+    EyeColor["brown"] = "Common eye color";
+    EyeColor["blue"] = "Rare eye color";
+    EyeColor["green"] = "Very rare eye color";
+})(EyeColor || (EyeColor = {}));
+function displayStudent(student) {
+    console.log(student.name + "\n" + student.age + "\n" + student.eyeColor);
+}
+displayStudent({
+    name: "Jack",
+    age: 20,
+    eyeColor: EyeColor.brown,
+});
+// ps -> If you want to a property to be optional, add a "?" after the name, Ex: age?: 29;
--- index.ts
+++ index.ts
@@ -68,16 +68,90 @@ let newCar: Car1 = {
 // -------------------------------------------------------------------------------------------------------------------------------------------------------------------------------//
 
 // UNION -> Let's a variable holds more than one value;
-let x: string | number = "Hello World"; 
+let x: string | number = "Hello World";
 /* In this case we are saying that the variable "x" can have two different types of value, a string, or a number, and the or is used with the Union icon(|)! ps: yo can add more than... 
 2 types */
 
-type occupation = "employed" | "student"
-let jackOccupation: occupation = "employed"
+type occupation = "employed" | "student";
+let jackOccupation: occupation = "employed";
 /* Combining Custom Types with Union, we can create this! We are saying that the custom type occupation can actually have more than one result(2 in this case), either the string...
 "employed" or the string "student", if the result of a variable that uses the occupation type is different than those two, we have an error!
 */
 
 // -------------------------------------------------------------------------------------------------------------------------------------------------------------------------------//
 
-// TYPE ASSERTION ->
+// TYPE ASSERTION -> Let's say we create a variable called y with the type of any, that initially is a string. With Type Assertion we are able to specify the type of the change!
+let y: any = "a";
+y = <number>22;
+
+// -------------------------------------------------------------------------------------------------------------------------------------------------------------------------------//
+
+// FUNCTIONS ->
+
+// How to write function that returns something -> the parameters should have their type specified, and after that we need to write what type will be the result of the function;
+function add(x: number, y: number): number {
+  return x + y;
+}
+add(2, 5);
+
+// How to write function that don't return anything ->
+function sayHi(msg: string): void {
+  console.log(msg);
+}
+sayHi("hey");
+
+// -------------------------------------------------------------------------------------------------------------------------------------------------------------------------------//
+
+// GENERICS ->
+
+/* In this case we are creating a function that takes an array and returns a copy of that array. We added three T's, which are things called "generics", creating a... 
+placeholder to the function called T(it can be anything). So when we create the numCopy and strCopy variables, inside it we call the function, and in the place of the T, we set the...
+type(T) to the type in question, followed by the parameter(array with T's, where in numCopy is a number, and in strCopy is a string!);
+
+ps -> the first T is to create the placeholder, the second one is to specify the type of the array which is an array filled with T, and of course the third T is the result, an array...
+filled with T;
+
+*/
+function copyArr<T>(arr: T[]): T[] {
+  return [...arr];
+}
+let numCopy = copyArr<number>([1, 2, 3]);
+let strCopy = copyArr<string>(["1", "2", "3"]);
+
+// -------------------------------------------------------------------------------------------------------------------------------------------------------------------------------//
+
+/* ENUMS(enumerations) -> A feature in TypeScript that allows you to define a set of named constants in a object. So we start creatin an enum object called EyeColor with colors as...
+keys inside the object. We are going to create a function that displays in the console a student given information(including the eye color). So we create the function displayStudent...
+getting as a parameter, a "student" with the type of Student(interface), that is an object filled with three different properties, name as a string, age as a number and EyeColor as...
+the EyeColor enum object. So back to the displayStudent function we console.log the three expected properties, we call the function passing an object with the three expected keys, the...
+eyeColor specified as the brown key inside the EyeColor enum object! And the result at the console log is: 
+
+Jack
+20
+Common eye color
+
+*/
+
+enum EyeColor {
+  brown = "Common eye color",
+  blue = "Rare eye color",
+  green = "Very rare eye color",
+}
+
+interface Student {
+  name: string;
+  age: number;
+  eyeColor: EyeColor;
+}
+
+function displayStudent(student: Student): void {
+  console.log(student.name + "\n" + student.age + "\n" + student.eyeColor);
+}
+
+displayStudent({
+  name: "Jack",
+  age: 20,
+  eyeColor: EyeColor.brown,
+});
+
+// ps -> If you want to a property to be optional, add a "?" after the name, Ex: age?: 29;
