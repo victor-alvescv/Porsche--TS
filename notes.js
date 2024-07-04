4ac84f97bbe99c8b002ed25a4b08392cb30a29f2
Victor Alves <victoralvescabral7@gmail.com>
2024-07-04 02:38:29 -0300

    Final Details

--- notes.js
+++ notes.js
@@ -13,13 +13,11 @@ WHY SHOULD WE USE IT? ->
 . Catch errors in editor rather than in production;
 . Very popular among tech companies;
 
-HOW TO SET IT UP -> Write "npm install -g typescript" in the terminal, and than create a new folder. To create a TypeScript file write ts at the end
+HOW TO SET IT UP -> Write "npm install -g typescript" in the terminal, and than create a new folder. To create a TypeScript file write ts at the end...
 
 . When you write "tsc index"(tsc typescript-file-name) in the terminal, a file called index.js will appear, that is the TS compiler, that is compiling the TS code into JS. After doing...
 that, you will see that the index.js file is using an old type of code, so write in the terminal "tsc --init" to create a new tsconfig.json file, that is used to configer the behavior...
 of the compiler. After that just write "tsc" in the terminal, it should change the weird old code to a new one. But to the tsc code to work(the compiler to work), we don't want to...
 write that all the time we do a change, so write "tsc --watch" 
-
-12:47 frontend Simplified! and after the web dev simplified typescript playlist
 
 */
